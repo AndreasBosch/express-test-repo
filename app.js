70605e85671322bf08795a1ca84643bff4ebff05
AndreasBosch <5123668@rts-365.dk>
2018-02-02 13:29:12 +0100

    Seje ændringer

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
+
 
 const logger = require('morgan');
 app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.get('/api/test', (req, res) => {
-    res.json({
-        greeting: "Hello World"
-    });
-});
+app.get('/api/test/:id', (req, res) => {
+    console.log(req.query);
+    res.json({ text: "Hello Worlds, et eller andet"});
+ });
 
 app.get('/api/person', (req, res) => {
     res.json({
         navn: "Andreas",
-        efternavn: "Bosch",
+        efternavn: "Bosch",d
         by: "Frederiksberg",
         alder: 19,
     });
